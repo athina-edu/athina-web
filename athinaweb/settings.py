a92cc5d7d7702b21de56b2e3c6e0f960f393175b
Unknown <tsikerdekis@gmail.com>
2019-04-12 17:31:01 -0700

    Fix

--- athinaweb/settings.py
+++ athinaweb/settings.py
@@ -117,7 +117,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static_files")
 
 INTERNAL_IPS = '127.0.0.1'
 
