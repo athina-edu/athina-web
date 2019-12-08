7cb25e0598cf3c43aa7a14c05c8ec153dfd71e49
Michael <tsikerdekis@gmail.com>
2019-12-07 18:37:07 -0800

    Preping

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='athinaweb',
-    version='0.9',
+    version='0.97',
     packages=find_packages(),
     scripts=['manage.py'],
     install_requires=['gitpython', 'Django>=2.0,<3.0', 'djangorestframework', 'django-registration-redux',
