ba67c466077417a0f7948ddc84307b6d15f42755
Michael <tsikerdekis@gmail.com>
2019-12-09 09:16:12 -0800

    Django full mysql support

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     scripts=['manage.py'],
     install_requires=['gitpython', 'Django>=2.0,<3.0', 'djangorestframework', 'django-registration-redux',
-                      'python-dateutil', 'gunicorn', 'pymysql', 'pyyaml'],
+                      'python-dateutil', 'gunicorn', 'pymysql', 'pyyaml', 'mysqlclient'],
     url='https://github.com/athina-edu/athina-web',
     license='MIT',
     author='Michail Tsikerdekis',
